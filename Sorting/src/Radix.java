7b19d8b9db688bb52a9228c9ee25196d87a7903d
Serhiy0406 <pavchuk.serhiy@chnu.edu.ua>
2022-03-11 16:24:59 +0200

    reformed code

--- Sorting/src/Radix.java
+++ Sorting/src/Radix.java
@@ -5,53 +5,55 @@ public class Radix {
         ArrayPrint(arr);
         int max = FindMax(arr);
         int x = (int) Math.ceil(Math.log10(max));
-        for(int i=0; i<x; i++){
+        for (int i = 0; i < x; i++) {
             arr = SortArray(arr, i);
         }
         ArrayPrint(arr);
     }
 
-    public static int FindMax(int[] arr){
+    public static int FindMax(int[] arr) {
         int max = -10;
-        for(int i=0; i < arr.length;i++){
-            if(arr[i]> max){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
                 max = arr[i];
             }
         }
         return max;
     }
-    public static void ArrayPrint(int[] arr){
+
+    public static void ArrayPrint(int[] arr) {
         System.out.print("(");
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.print(")");
         System.out.println("");
     }
-    public static int[] SortArray(int[] arr, int x){
+
+    public static int[] SortArray(int[] arr, int x) {
         int[] temp = new int[arr.length];
         int[] countArr;
         int[] outputArr = new int[arr.length];
-        for(int i=0; i< arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             outputArr[i] = arr[i];
-            temp[i] = arr[i]/(int)(Math.pow(10, x))%10;
+            temp[i] = arr[i] / (int) (Math.pow(10, x)) % 10;
         }
         int max = FindMax(temp);
-        countArr = new int[max+1];
-        for(int i=0; i < countArr.length; i++){
+        countArr = new int[max + 1];
+        for (int i = 0; i < countArr.length; i++) {
             int count = 0;
-            for(int j=0; j < temp.length; j++){
-                if(temp[j] == i){
+            for (int j = 0; j < temp.length; j++) {
+                if (temp[j] == i) {
                     count++;
                 }
             }
-            countArr[i]=count;
+            countArr[i] = count;
         }
-        for(int i=1; i< countArr.length; i++){
-            countArr[i] += countArr[i-1];
+        for (int i = 1; i < countArr.length; i++) {
+            countArr[i] += countArr[i - 1];
         }
         //int i=0; i<temp.length;i++ don't worked
-        for(int i = temp.length - 1; i >= 0; i--){
+        for (int i = temp.length - 1; i >= 0; i--) {
             outputArr[countArr[temp[i]] - 1] = arr[i];
             countArr[temp[i]]--;
         }
