66b13ae6255777dc8e5c55044e92a7bb3ddf5cb2
Serhiy0406 <pavchuk.serhiy@chnu.edu.ua>
2022-03-13 18:24:44 +0200

    Add taxi class

---
+++ ClassProject/src/Taxi.java
@@ -0,0 +1,11 @@
+public class Taxi {
+    private String mark;
+    private String phone;
+    private int prizePerM;
+    public void setPhone (String phone){
+        this.phone = phone;
+    }
+    public String getPhone(){
+        return this.phone;
+    }
+}
